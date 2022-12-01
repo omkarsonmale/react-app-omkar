086744d4fa4bec2ac3de59d174fe75eacb393ed7
omkarsonmale <103109992+omkarsonmale@users.noreply.github.com>
2022-12-01 22:12:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useContext } from 'react';
+import './App.css';
+import FirstStep from './Components/FirstStep';
+import SecondStep from './Components/SecondStep';
+import ThirdStep from './Components/ThirdStep';
+import { Stepper, StepLabel, Step } from '@material-ui/core';
+import { multiStepContext } from './StepContext';
+import DisplayData from './Components/DisplayData';
+
+function App() {
+
+  const { currentStep, finalData } = useContext(multiStepContext);
+
+  function showStep(step) {
+    switch (step) {
+      case 1:
+        return <FirstStep />
+      case 2:
+        return <SecondStep />
+      case 3:
+        return <ThirdStep />
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <h3 style={{ color: 'red', textDecoration: 'underline' }}>ReactJS Multi Step Application</h3> */}
+
+        <div className='center-stepper'>
+          <Stepper style={{ width: '18%' }} activeStep={currentStep - 1} orientation="horizontal">
+            <Step>
+              <StepLabel></StepLabel>
+            </Step>
+            <Step>
+              <StepLabel></StepLabel>
+            </Step>
+            <Step>
+              <StepLabel></StepLabel>
+            </Step>
+          </Stepper>
+        </div>
+        {showStep(currentStep)}
+        <br />
+        {finalData.length > 0 ? <DisplayData /> : ''}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
