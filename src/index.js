761d5d94444dd1c2943facb72848708423bab41b
omkarsonmale <103109992+omkarsonmale@users.noreply.github.com>
2022-12-01 22:04:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import StepContext from './StepContext';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StepContext>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </StepContext>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
